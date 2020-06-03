edcc430bf69c207c0f6bf408781baa1d3cba57c8
newpages28 <60360725+newpages28@users.noreply.github.com>
2020-06-03 17:34:54 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const defaultResult=0;
+let currentResult=defaultResult;
+currentResult=(currentResult + 10)*3/2-1;
+let calculationDescription=`(${defaultResult} + 10)*3/2-1`;
+console.log(currentResult);
+console.log(calculationDescription);
+outputResult(111, 222);
+// outputResult(currentResult, calculationDescription);
+
